badae18f537495e135aec1d7ea34a7e088cb1751
unknown <caylenepicache@gmail.com>
2018-05-12 11:48:35 -0700

    initialize files part 2

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.static("public"));
 
 //ROUTES
 require("./routes/html-routes.js")(app);
-require("./routes/bank-api-routes.js")(app);
-//require("./routes/post-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/account-api-routes.js")(app);
+require("./routes/transaction-api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
